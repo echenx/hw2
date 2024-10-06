72f82f6a6eb44a3d2581ff3aa2e40310a5b4f3f0
evan <evanchenx7@gmail.com>
2024-10-06 03:35:00 -0400

    display nums and operators

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,41 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(),
+      home: const CalculatorPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class CalculatorPage extends StatefulWidget {
+  const CalculatorPage({super.key});
+
+  @override
+  _CalculatorPageState createState() => _CalculatorPageState();
+}
+
+class _CalculatorPageState extends State<CalculatorPage> {
+  String _displayText = '';
+  String _first = '';
+  String _second = '';
+  String _operator = '';
+  bool _operatorSelected = false;
+
+  void _onPress(String value) {
+    setState(() {
+      if (RegExp(r'^[0-9]$').hasMatch(value)) { //check if the button pressed is a num
+        if (_operatorSelected) {
+          _second += value;
+        } else {
+          _first += value;
+        }
+        _displayText += value;
+      } else if (RegExp(r'^[+\-*/]$').hasMatch(value) && _first.isNotEmpty) { //check if the button pressed is a operator 
+        _operator = value;
+        _operatorSelected = true;
+        _displayText += ' $value ';
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,16 +62,16 @@ class MyHomePage extends StatelessWidget {
           Container(
             padding: const EdgeInsets.all(20),
             alignment: Alignment.centerRight,
-            child: const Text(
-              '',
-              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+            child: Text(
+              _displayText,
+              style: const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
             ),
           ),
           const SizedBox(height: 10),
           _button(['7', '8', '9', '/']),
           _button(['4', '5', '6', '*']),
           _button(['1', '2', '3', '+']),
-          _button(['0', 'C', '=', '-']),
+          _button(['0', 'C', '=', '-']), 
         ],
       ),
     );
@@ -59,20 +87,24 @@ class MyHomePage extends StatelessWidget {
   }
 
   Widget _buildButton(String value) {
-    return Container(
-      height: 100,
-      width: 100,
-      margin: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Center(
-        child: Text(
-          value,
-          style: const TextStyle(fontSize: 32, color: Colors.white),
+    return InkWell(
+      onTap: () => _onPress(value), 
+      child: Container(
+        height: 100,
+        width: 100,
+        margin: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Center(
+          child: Text(
+            value,
+            style: const TextStyle(fontSize: 32, color: Colors.white),
+          ),
         ),
       ),
     );
   }
 }
+
